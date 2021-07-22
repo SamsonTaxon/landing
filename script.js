b232e185ab51be62e27ddf6bc86c1da0ad1e3a77
SamsonT <25094935+SamsonTaxon@users.noreply.github.com>
2021-07-21 18:48:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var $ = function(id) {
+  return document.getElementById(id);
+};
+var inc = 0;
+var out = 0;
+var str = 'SAMSON TAXON';
+var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
+var t;
+
+var anim = function() {
+  inc++;
+  if (inc % 7 === 0 && out < str.length) {
+    $('anim').appendChild(document.createTextNode(str[out]));
+    out++;
+  } else if (out >= str.length) {
+    $('shuffle').innerHTML = '';
+    removeInterval(t);
+  }
+  $('shuffle').innerHTML =
+    chars[Math.floor(Math.random() * chars.length)];
+};
+t = setInterval(anim, 50);
+$('anim').innerHTML = '';
