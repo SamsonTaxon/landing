8c0a12574964a450cd6cffe5961e953c980aaa12
Samson Taxon <samsontaxon@usc-guestwireless-upc-newsc21948.usc.edu>
2021-10-19 17:34:43 -0700

    darkmode

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ var anim = function() {
 };
 t = setInterval(anim, 50);
 $('anim').innerHTML = '';
+
+
+var toggle = document.getElementById("theme-toggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
