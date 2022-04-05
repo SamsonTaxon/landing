099e5ca1cba7cb80b572b33e47163706dd135142
Samson Taxon <samsontaxon@MacBook-Pro-5.local>
2022-04-05 11:38:06 -0700

    portfolio

---
+++ Portfolio/content.js
@@ -0,0 +1,96 @@
+function lucent() {
+    // document.getElementById("heading").innerHTML = "Lucent";
+    // document.getElementById("description").innerHTML = "This is an UV Sanitizing lightbulb. This was designed for an Innovators Forum pitch. Cad was done in Fusion 360.";
+    // document.getElementById("picture").src="WorkThumbnails/lucent_bulb.png";
+    
+    document.getElementById("contentFrame").src="Items/lucent/lucent.html";
+    document.getElementById('modal').style.display='block';
+}
+function lucentBox() {
+    document.getElementById("contentFrame").src="Items/lucent_box/lucent_box.html";
+    document.getElementById('modal').style.display='block';
+}
+function beats() {
+    document.getElementById("contentFrame").src="Items/beats/beats.html";
+    document.getElementById('modal').style.display='block';
+}
+function coaster() {
+    document.getElementById("contentFrame").src="Items/coaster/coaster.html";
+    document.getElementById('modal').style.display='block';
+}
+function desk() {
+    document.getElementById("contentFrame").src="Items/desk/desk.html";
+    document.getElementById('modal').style.display='block';
+}
+function extruder1() {
+    document.getElementById("contentFrame").src="Items/extruder/extruder1.html";
+    document.getElementById('modal').style.display='block';
+}
+function extruder2() {
+    document.getElementById("contentFrame").src="Items/extruder2/extruder2.html";
+    document.getElementById('modal').style.display='block';
+}
+function gameboy() {
+    document.getElementById("contentFrame").src="Items/Gameboy/gameboy.html";
+    document.getElementById('modal').style.display='block';
+}
+function grp() {
+    document.getElementById("contentFrame").src="Items/grp/grp.html";
+    document.getElementById('modal').style.display='block';
+}
+function magnet1() {
+    document.getElementById("contentFrame").src="Items/magnet1/magnet1.html";
+    document.getElementById('modal').style.display='block';
+}
+function magnet2() {
+    document.getElementById("contentFrame").src="Items/magnet2/magnet2.html";
+    document.getElementById('modal').style.display='block';
+}
+function psa() {
+    document.getElementById("contentFrame").src="Items/psa/psa.html";
+    document.getElementById('modal').style.display='block';
+}
+function rings() {
+    document.getElementById("contentFrame").src="Items/rings/rings.html";
+    document.getElementById('modal').style.display='block';
+}
+function rocket() {
+    document.getElementById("contentFrame").src="Items/rocket/rocket.html";
+    document.getElementById('modal').style.display='block';
+}
+function screw() {
+    document.getElementById("contentFrame").src="Items/screw/screw.html";
+    document.getElementById('modal').style.display='block';
+}
+function skate() {
+    document.getElementById("contentFrame").src="Items/skate/skate.html";
+    document.getElementById('modal').style.display='block';
+}
+function soldering() {
+    document.getElementById("contentFrame").src="Items/soldering/soldering.html";
+    document.getElementById('modal').style.display='block';
+}
+function sunset() {
+    document.getElementById("contentFrame").src="Items/sunset/sunset.html";
+    document.getElementById('modal').style.display='block';
+}
+function theranos() {
+    document.getElementById("contentFrame").src="Items/theranos/theranos.html";
+    document.getElementById('modal').style.display='block';
+}
+function tommy() {
+    document.getElementById("contentFrame").src="Items/tommy/tommy.html";
+    document.getElementById('modal').style.display='block';
+}
+function wallet() {
+    document.getElementById("contentFrame").src="Items/wallet/wallet.html";
+    document.getElementById('modal').style.display='block';
+}
+function wcc1() {
+    document.getElementById("contentFrame").src="Items/wcc1/wcc1.html";
+    document.getElementById('modal').style.display='block';
+}
+function lamp() {
+    document.getElementById("contentFrame").src="Items/lamp/lamp.html";
+    document.getElementById('modal').style.display='block';
+}
